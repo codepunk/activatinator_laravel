ac28092d688a1e13eb2c3192580f6839e84ebb89
Scott Slater <slaterama@gmail.com>
2018-06-15 14:10:13 -0400

    Added ClearActivationsCommand and changed auth.activatinator accessors to codepunk.activatinator

---
+++ src/Console/ClearActivationsCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Console\Command;
+
+namespace Codepunk\Activatinator\Console;
+
+class ClearActivationsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'activatinator:clear-activations {name? : The name of the activatinator broker}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Flush expired activation tokens';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->laravel['codepunk.activatinator']->broker($this->argument('name'))->getRepository()->deleteExpired();
+
+        $this->info('Expired activation tokens cleared!');
+    }
+}
+
